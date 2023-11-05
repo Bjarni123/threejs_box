d5611bd2e696bf903586df9563f6fb6dd4f64950
Bjarni123 <bjarni04@gmail.com>
2023-11-05 23:37:25 +0000

    played around with stuff

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,11 +1,16 @@
 import { World } from './world.js';
 
-function main () {
-    const container = document.querySelector('#scene-conatiner');
+// create the main function
+function main() {
+    // Get a reference to the container element
+    const container = document.querySelector('#scene-container');
 
+    // 1. Create an instance of the World app
     const world = new World(container);
 
+    // 2. Render the scene
     world.render();
 }
 
+// call main to start the app
 main();
--- JAVASCRIPT/world.js
+++ JAVASCRIPT/world.js
@@ -1,7 +1,34 @@
-class World {
-    constructor(container) {}
+import { createCamera } from './camera.js';
+import { createCube } from './cube.js';
+import { createScene } from './scene.js';
 
-    render() {}
+import { createRenderer } from './renderer.js';
+import { Resizer } from './resizer.js';
+
+// These variables are module-scoped: we cannot access them
+// from outside the module
+let camera;
+let renderer;
+let scene;
+
+class World {
+    constructor(container) {
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        container.append(renderer.domElement);
+
+        const cube = createCube();
+
+        scene.add(cube);
+
+        const resizer = new Resizer(container, camera, renderer);
+    }
+
+    render() {
+        // draw a single frame
+        renderer.render(scene, camera);
+    }
 }
 
 export { World };
